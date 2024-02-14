acd95bb9a370155646746e5aaa588151c3587195
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-02-14 23:12:40 +0400

    Added serializable to navigate behavior

--- coreapp/src/commonMain/kotlin/com/metacto/core/navigation/NavigateBehaviour.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/navigation/NavigateBehaviour.kt
@@ -1,6 +1,11 @@
 package com.metacto.core.navigation
 
-enum class NavigateBehaviour {
+import com.metacto.core.utils.CommonParcelable
+import com.metacto.core.utils.CommonSerializable
+import dev.icerock.moko.parcelize.Parcelize
+
+@Parcelize
+enum class NavigateBehaviour : CommonParcelable, CommonSerializable {
     Normal,
     ReplaceIfCurrent,
     KeepIfCurrent
