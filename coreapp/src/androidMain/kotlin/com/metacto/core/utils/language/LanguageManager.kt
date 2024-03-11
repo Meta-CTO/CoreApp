614773ddc6d8c70f2f3309b0ddeb7d3b39b65110
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2024-03-11 22:28:30 +0200

    Add isoLang

--- coreapp/src/androidMain/kotlin/com/metacto/core/utils/language/LanguageManager.kt
+++ coreapp/src/androidMain/kotlin/com/metacto/core/utils/language/LanguageManager.kt
@@ -1,10 +1,15 @@
 package com.metacto.core.utils.language
 
+import com.metacto.core.utils.extensions.nullIfEmpty
 import java.util.Locale
 
 class LanguageManager : ILanguageManager {
 
     override fun getDeviceIso3Lang(): String {
         return Locale.getDefault().isO3Language
+    }
+
+    override fun getDeviceIso2Lang(): String {
+        return Locale.getDefault().language.nullIfEmpty() ?: "en"
     }
 }
