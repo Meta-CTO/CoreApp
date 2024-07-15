7d4bd4cde06332bd5b074ff997d2955b6adff195
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-07-15 21:06:30 +0300

    Updated PickerItem to be serializable

--- coreapp/src/commonMain/kotlin/com/metacto/core/presentation/itemPicker/models/PickerItem.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/presentation/itemPicker/models/PickerItem.kt
@@ -1,8 +1,9 @@
 package com.metacto.core.presentation.itemPicker.models
 
 import com.metacto.core.utils.CommonParcelable
+import com.metacto.core.utils.CommonSerializable
 
-interface PickerItem : CommonParcelable {
+interface PickerItem : CommonParcelable, CommonSerializable {
     val key: String
     val title: String
 }
