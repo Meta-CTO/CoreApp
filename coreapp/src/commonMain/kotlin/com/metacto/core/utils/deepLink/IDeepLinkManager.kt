ad44107504ace47b5dacc1e1bd2caee2b0ae382a
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-08-04 22:25:16 +0400

    Created DeepLinkManager

--- coreapp/src/commonMain/kotlin/com/metacto/core/utils/deepLink/IDeepLinkManager.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/utils/deepLink/IDeepLinkManager.kt
@@ -1,5 +1,7 @@
 package com.metacto.core.utils.deepLink
 
+val Class = IDeepLinkManager::class
+
 interface IDeepLinkManager {
     fun handleDeepLink(link: String)
     fun handleDynamicLink(link: String)
