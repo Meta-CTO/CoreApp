0b5792ef2d9d6f970dea5121a322483252f5d981
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-10-11 21:58:57 +0400

    Fixed upload video issue.

--- coreapp/src/commonMain/kotlin/com/metacto/core/domain/repos/UploadRepository.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/domain/repos/UploadRepository.kt
@@ -8,6 +8,7 @@ import com.metacto.strapikmm.constants.SharedConstants
 import com.metacto.strapikmm.datasource.network.services.strapi.JsonFlatter
 import com.metacto.strapikmm.datasource.network.services.strapi.StrapiService
 import com.metacto.strapikmm.datasource.network.services.strapi.convert
+import com.metacto.strapikmm.model.PagingResponse
 import com.metacto.strapikmm.model.image.Image
 import com.metacto.strapikmm.sharedpreference.KmmPreference
 import io.ktor.client.call.body
@@ -43,7 +44,7 @@ class UploadRepository(
         )
 
         return if (previewUrl != null && video.id != null) {
-            updateVideoPreviewUrl(video.id!!, previewUrl)
+            updateVideoPreviewUrl(video.id!!, previewUrl) ?: video
         } else {
             video
         }
@@ -71,11 +72,11 @@ class UploadRepository(
     }
 
     @Throws(Throwable::class)
-    suspend fun updateVideoPreviewUrl(id: Int, previewUrl: String): Image {
-        return uploadService.put<Image> {
+    suspend fun updateVideoPreviewUrl(id: Int, previewUrl: String): Image? {
+        return uploadService.put<PagingResponse<Image>> {
             endpoint("/custom-uploader/{id}")
             path("id", id.toString())
             body(UpdatePreviewUrlRequest(UpdatePreviewUrlRequestData(previewUrl)))
-        }
+        }.data.firstOrNull()
     }
 }
