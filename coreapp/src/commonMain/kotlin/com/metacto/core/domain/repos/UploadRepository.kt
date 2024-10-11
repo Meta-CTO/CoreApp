316660884700f3c1803ffef114cdb06ccec591ee
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-10-11 22:36:29 +0400

    Upload Fixes

--- coreapp/src/commonMain/kotlin/com/metacto/core/domain/repos/UploadRepository.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/domain/repos/UploadRepository.kt
@@ -8,7 +8,6 @@ import com.metacto.strapikmm.constants.SharedConstants
 import com.metacto.strapikmm.datasource.network.services.strapi.JsonFlatter
 import com.metacto.strapikmm.datasource.network.services.strapi.StrapiService
 import com.metacto.strapikmm.datasource.network.services.strapi.convert
-import com.metacto.strapikmm.model.PagingResponse
 import com.metacto.strapikmm.model.image.Image
 import com.metacto.strapikmm.sharedpreference.KmmPreference
 import io.ktor.client.call.body
@@ -43,11 +42,11 @@ class UploadRepository(
             fileName = "$fileName.mp4"
         )
 
-        return if (previewUrl != null && video.id != null) {
-            updateVideoPreviewUrl(video.id!!, previewUrl) ?: video
-        } else {
-            video
+        if (previewUrl != null && video.id != null) {
+            updateVideoPreviewUrl(video.id!!, previewUrl)
         }
+
+        return video
     }
 
     @Throws(Throwable::class)
@@ -72,11 +71,11 @@ class UploadRepository(
     }
 
     @Throws(Throwable::class)
-    suspend fun updateVideoPreviewUrl(id: Int, previewUrl: String): Image? {
-        return uploadService.put<PagingResponse<Image>> {
+    suspend fun updateVideoPreviewUrl(id: Int, previewUrl: String) {
+        uploadService.put<Unit> {
             endpoint("/custom-uploader/{id}")
             path("id", id.toString())
             body(UpdatePreviewUrlRequest(UpdatePreviewUrlRequestData(previewUrl)))
-        }.data.firstOrNull()
+        }
     }
 }
