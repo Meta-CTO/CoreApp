1407d19056be92046d9485cc19563b93502ed3fa
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-10-16 00:34:46 +0400

    Added more int extensions

--- coreapp/src/commonMain/kotlin/com/metacto/core/utils/extensions/NumberExtensions.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/utils/extensions/NumberExtensions.kt
@@ -140,7 +140,7 @@ fun Int.toHex(): String {
     return this.toString(16).padStart(2, '0').uppercase()
 }
 
-fun Int.formatTimeMMSS(): String {
+fun Int.formatSecondsToMMSS(): String {
     val sec = this % 60
     val minutes = (this / 60) % 60
 
@@ -148,6 +148,18 @@ fun Int.formatTimeMMSS(): String {
     val secondsStr = if (sec < 10) "0$sec" else "$sec"
 
     return "$minutesStr:$secondsStr"
+}
+
+fun Int.formatSecondsToHHMMSS(): String {
+    val sec = this % 60
+    val minutes = (this / 60) % 60
+    val hours = (this / 3600)
+
+    val hoursStr = if (hours < 10) "0$hours" else "$hours"
+    val minutesStr = if (minutes < 10) "0$minutes" else "$minutes"
+    val secondsStr = if (sec < 10) "0$sec" else "$sec"
+
+    return "$hoursStr:$minutesStr:$secondsStr"
 }
 
 expect fun Double.format(decimalsCount: Int): String
