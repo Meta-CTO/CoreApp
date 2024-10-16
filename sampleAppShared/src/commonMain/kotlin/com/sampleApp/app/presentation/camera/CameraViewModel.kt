60c2328266297c3184c14899f89e6f0c9179c0be
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-10-16 08:38:47 +0400

    Camera screen fixes.

--- sampleAppShared/src/commonMain/kotlin/com/sampleApp/app/presentation/camera/CameraViewModel.kt
+++ sampleAppShared/src/commonMain/kotlin/com/sampleApp/app/presentation/camera/CameraViewModel.kt
@@ -75,8 +75,10 @@ class CameraViewModel : BaseViewModel<State, Event, Effect>() {
     private fun handleToggleRecord() = executeCatching(
         context = Dispatchers.IO,
         block = {
+            println("Toggle record")
             val cameraController = currentState.cameraController ?: return@executeCatching
             if (currentState.isRecording) {
+                println("Stop recording")
                 val result = cameraController.stopRecording()
                 setState {
                     copy(
@@ -84,6 +86,8 @@ class CameraViewModel : BaseViewModel<State, Event, Effect>() {
                         recordingFilePath = result.videoPath
                     )
                 }
+                println("Stopped recording")
+                println("Video path: ${result.videoPath}")
                 currentState.videoController?.play()
 
                 // Upload it
@@ -102,11 +106,15 @@ class CameraViewModel : BaseViewModel<State, Event, Effect>() {
                 }
 
             } else {
+                println("Start recording")
                 cameraController.recordVideo(
                     params = VideoRecordingParams()
                 )
                 setState { copy(isRecording = true) }
             }
+        },
+        onError = { throwable, msg ->
+            println("throwable: $throwable, msg: $msg")
         }
     )
 
