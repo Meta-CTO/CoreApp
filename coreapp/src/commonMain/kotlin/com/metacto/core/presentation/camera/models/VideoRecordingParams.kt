d961d8db7c4cb981da570f49642bafb71446513c
Mahmoud Elshamy <shamyyoun@gmail.com>
2024-12-18 23:05:04 +0400

    Fixed camera recorder audio issue in iOS.

--- coreapp/src/commonMain/kotlin/com/metacto/core/presentation/camera/models/VideoRecordingParams.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/presentation/camera/models/VideoRecordingParams.kt
@@ -1,5 +1,5 @@
 package com.metacto.core.presentation.camera.models
 
 data class VideoRecordingParams(
-    val fileName: String = "recorded_video.mp4"
+    val fileName: String = "recorded_video.mov"
 )
