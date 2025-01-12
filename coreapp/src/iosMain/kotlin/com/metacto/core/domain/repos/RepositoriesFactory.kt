e3292232899ed9a06f02fcd10cae4bae48a541d8
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-01-12 23:24:55 +0200

    Pref migration

--- coreapp/src/iosMain/kotlin/com/metacto/core/domain/repos/RepositoriesFactory.kt
+++ coreapp/src/iosMain/kotlin/com/metacto/core/domain/repos/RepositoriesFactory.kt
@@ -1,5 +1,6 @@
 @file:OptIn(
-    ExperimentalForeignApi::class, ExperimentalSettingsApi::class,
+    ExperimentalForeignApi::class,
+    ExperimentalSettingsApi::class,
     ExperimentalSettingsImplementation::class
 )
 
@@ -15,7 +16,11 @@ import com.russhwolf.settings.ExperimentalSettingsImplementation
 import com.russhwolf.settings.KeychainSettings
 import com.russhwolf.settings.NSUserDefaultsSettings
 import kotlinx.cinterop.ExperimentalForeignApi
+import platform.Foundation.CFBridgingRetain
 import platform.Foundation.NSUserDefaults
+import platform.Security.kSecAttrAccessible
+import platform.Security.kSecAttrAccessibleAfterFirstUnlock
+import platform.Security.kSecAttrService
 import kotlin.reflect.KClass
 
 // DON'T REMOVE COMMENTED CODE HERE, WE NEED TO MIGRATE OLD KEYCHAIN TO NEW KEYCHAIN
@@ -28,10 +33,10 @@ actual open class RepositoriesFactory<T : SerializableNetworkError> constructor(
 ) {
 
     private val oldKeyChainStore = KeychainSettings(service = appStorageName)
-//    private val newKeyChainStore = KeychainSettings(
-//        kSecAttrService to CFBridgingRetain(appStorageName),
-//        kSecAttrAccessible to kSecAttrAccessibleAfterFirstUnlock,
-//    )
+    private val newKeyChainStore = KeychainSettings(
+        kSecAttrService to CFBridgingRetain(appStorageName),
+        kSecAttrAccessible to kSecAttrAccessibleAfterFirstUnlock,
+    )
 
     actual val sharedPreference = KmmPreference(
         preferences = NSUserDefaultsSettings(NSUserDefaults.standardUserDefaults()),
@@ -50,41 +55,41 @@ actual open class RepositoriesFactory<T : SerializableNetworkError> constructor(
         kmmPreference = sharedPreference
     )
 
-//    init {
-//        migrate(oldKeyChainStore, newKeyChainStore)
-//    }
+    init {
+        migrate(oldKeyChainStore, newKeyChainStore)
+    }
 }
 
 // DON'T REMOVE COMMENTED CODE HERE, WE NEED TO MIGRATE OLD KEYCHAIN TO NEW KEYCHAIN
 // WE HAVE TO WAIT FOR THE NEW KEYCHAIN TO BE IMPLEMENTED IN THE LIBRARY FIRST
 
-//private fun migrate(oldKeyChainStore: KeychainSettings, newKeyChainStore: KeychainSettings) {
-//    if (newKeyChainStore.hasKey("_version")) return
-//
-//    oldKeyChainStore.keys.forEach { key ->
-//        // Check each type and store accordingly
-//        val oldValue: Any? = when {
-//            oldKeyChainStore.getIntOrNull(key) != null -> oldKeyChainStore.getIntOrNull(key)
-//            oldKeyChainStore.getLongOrNull(key) != null -> oldKeyChainStore.getLongOrNull(key)
-//            oldKeyChainStore.getStringOrNull(key) != null -> oldKeyChainStore.getStringOrNull(key)
-//            oldKeyChainStore.getFloatOrNull(key) != null -> oldKeyChainStore.getFloatOrNull(key)
-//            oldKeyChainStore.getDoubleOrNull(key) != null -> oldKeyChainStore.getDoubleOrNull(key)
-//            oldKeyChainStore.getBooleanOrNull(key) != null -> oldKeyChainStore.getBooleanOrNull(key)
-//            else -> null
-//        }
-//
-//        oldValue?.let {
-//            when (it) {
-//                is Int -> newKeyChainStore.putInt(key, it)
-//                is Long -> newKeyChainStore.putLong(key, it)
-//                is String -> newKeyChainStore.putString(key, it)
-//                is Float -> newKeyChainStore.putFloat(key, it)
-//                is Double -> newKeyChainStore.putDouble(key, it)
-//                is Boolean -> newKeyChainStore.putBoolean(key, it)
-//            }
-//        }
-//    }
-//
-//    oldKeyChainStore.clear()
-//    newKeyChainStore.putString("_version", "1")
-//}
+private fun migrate(oldKeyChainStore: KeychainSettings, newKeyChainStore: KeychainSettings) {
+    if (newKeyChainStore.hasKey("_version")) return
+
+    oldKeyChainStore.keys.forEach { key ->
+        // Check each type and store accordingly
+        val oldValue: Any? = when {
+            oldKeyChainStore.getIntOrNull(key) != null -> oldKeyChainStore.getIntOrNull(key)
+            oldKeyChainStore.getLongOrNull(key) != null -> oldKeyChainStore.getLongOrNull(key)
+            oldKeyChainStore.getStringOrNull(key) != null -> oldKeyChainStore.getStringOrNull(key)
+            oldKeyChainStore.getFloatOrNull(key) != null -> oldKeyChainStore.getFloatOrNull(key)
+            oldKeyChainStore.getDoubleOrNull(key) != null -> oldKeyChainStore.getDoubleOrNull(key)
+            oldKeyChainStore.getBooleanOrNull(key) != null -> oldKeyChainStore.getBooleanOrNull(key)
+            else -> null
+        }
+
+        oldValue?.let {
+            when (it) {
+                is Int -> newKeyChainStore.putInt(key, it)
+                is Long -> newKeyChainStore.putLong(key, it)
+                is String -> newKeyChainStore.putString(key, it)
+                is Float -> newKeyChainStore.putFloat(key, it)
+                is Double -> newKeyChainStore.putDouble(key, it)
+                is Boolean -> newKeyChainStore.putBoolean(key, it)
+            }
+        }
+    }
+
+    oldKeyChainStore.clear()
+    newKeyChainStore.putString("_version", "1")
+}
