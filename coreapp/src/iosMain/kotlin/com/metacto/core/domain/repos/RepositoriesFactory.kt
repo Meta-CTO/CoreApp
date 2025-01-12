12f0ff8f25f188ff35461d0cd38dbf00db0a1876
ahmedelzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-01-12 23:31:57 +0200

    Add comments

--- coreapp/src/iosMain/kotlin/com/metacto/core/domain/repos/RepositoriesFactory.kt
+++ coreapp/src/iosMain/kotlin/com/metacto/core/domain/repos/RepositoriesFactory.kt
@@ -64,32 +64,71 @@ actual open class RepositoriesFactory<T : SerializableNetworkError> constructor(
 // WE HAVE TO WAIT FOR THE NEW KEYCHAIN TO BE IMPLEMENTED IN THE LIBRARY FIRST
 
 private fun migrate(oldKeyChainStore: KeychainSettings, newKeyChainStore: KeychainSettings) {
-    if (newKeyChainStore.hasKey("_version")) return
+    // Logger for debugging the migration process
+    val logger = Logger("RepositoriesFactory migrate")
 
+    // Check if the new store already has the "_version" key. If it does, migration is not needed.
+    if (newKeyChainStore.hasKey("_version")) {
+        logger.log("Migration skipped: newKeyChainStore already contains '_version'")
+        return
+    }
+
+    // Iterate over all keys in the old keychain store
     oldKeyChainStore.keys.forEach { key ->
-        // Check each type and store accordingly
+        logger.log("Processing key: $key")
+
+        // Determine the type of value associated with the key in the old keychain store
         val oldValue: Any? = when {
-            oldKeyChainStore.getIntOrNull(key) != null -> oldKeyChainStore.getIntOrNull(key)
-            oldKeyChainStore.getLongOrNull(key) != null -> oldKeyChainStore.getLongOrNull(key)
-            oldKeyChainStore.getStringOrNull(key) != null -> oldKeyChainStore.getStringOrNull(key)
-            oldKeyChainStore.getFloatOrNull(key) != null -> oldKeyChainStore.getFloatOrNull(key)
-            oldKeyChainStore.getDoubleOrNull(key) != null -> oldKeyChainStore.getDoubleOrNull(key)
-            oldKeyChainStore.getBooleanOrNull(key) != null -> oldKeyChainStore.getBooleanOrNull(key)
-            else -> null
+            oldKeyChainStore.getIntOrNull(key) != null -> {
+                logger.log("Key $key has type Int")
+                oldKeyChainStore.getIntOrNull(key)
+            }
+            oldKeyChainStore.getLongOrNull(key) != null -> {
+                logger.log("Key $key has type Long")
+                oldKeyChainStore.getLongOrNull(key)
+            }
+            oldKeyChainStore.getStringOrNull(key) != null -> {
+                logger.log("Key $key has type String")
+                oldKeyChainStore.getStringOrNull(key)
+            }
+            oldKeyChainStore.getFloatOrNull(key) != null -> {
+                logger.log("Key $key has type Float")
+                oldKeyChainStore.getFloatOrNull(key)
+            }
+            oldKeyChainStore.getDoubleOrNull(key) != null -> {
+                logger.log("Key $key has type Double")
+                oldKeyChainStore.getDoubleOrNull(key)
+            }
+            oldKeyChainStore.getBooleanOrNull(key) != null -> {
+                logger.log("Key $key has type Boolean")
+                oldKeyChainStore.getBooleanOrNull(key)
+            }
+            else -> {
+                logger.log("Key $key has an unknown or null value type")
+                null
+            }
         }
 
+        // If the old value is not null, migrate it to the new keychain store
         oldValue?.let {
+            logger.log("Migrating key: $key with value: $it")
+
             when (it) {
-                is Int -> newKeyChainStore.putInt(key, it)
-                is Long -> newKeyChainStore.putLong(key, it)
-                is String -> newKeyChainStore.putString(key, it)
-                is Float -> newKeyChainStore.putFloat(key, it)
-                is Double -> newKeyChainStore.putDouble(key, it)
-                is Boolean -> newKeyChainStore.putBoolean(key, it)
+                is Int -> newKeyChainStore.putInt(key, it).also { logger.log("Migrated key $key as Int") }
+                is Long -> newKeyChainStore.putLong(key, it).also { logger.log("Migrated key $key as Long") }
+                is String -> newKeyChainStore.putString(key, it).also { logger.log("Migrated key $key as String") }
+                is Float -> newKeyChainStore.putFloat(key, it).also { logger.log("Migrated key $key as Float") }
+                is Double -> newKeyChainStore.putDouble(key, it).also { logger.log("Migrated key $key as Double") }
+                is Boolean -> newKeyChainStore.putBoolean(key, it).also { logger.log("Migrated key $key as Boolean") }
             }
         }
     }
 
+    // Clear the old keychain store after migration
     oldKeyChainStore.clear()
+    logger.log("Old keychain store cleared")
+
+    // Mark the new keychain store as migrated by setting the "_version" key
     newKeyChainStore.putString("_version", "1")
+    logger.log("Migration complete: '_version' set in newKeyChainStore")
 }
