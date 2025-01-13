59136c79aa69657e3896591fc2480c6239eea0a7
Mahmoud Elshamy <shamyyoun@gmail.com>
2025-01-14 00:08:25 +0400

    Fixed phone number lib crash on iOS.

--- sampleAppShared/src/commonMain/kotlin/com/sampleApp/app/presentation/home/components/HomeContent.kt
+++ sampleAppShared/src/commonMain/kotlin/com/sampleApp/app/presentation/home/components/HomeContent.kt
@@ -12,6 +12,7 @@ import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.input.KeyboardType.Companion.Phone
 import androidx.compose.ui.unit.dp
 import com.metacto.core.presentation.components.buttons.PrimaryFilledButton
 import com.metacto.core.presentation.components.containers.ScreenColumn
@@ -19,9 +20,13 @@ import com.metacto.core.presentation.components.inputFields.PickerInputField
 import com.metacto.core.presentation.components.inputFields.PrimaryTextInputField
 import com.metacto.core.presentation.components.videoPlayer.ControlsType
 import com.metacto.core.presentation.components.videoPlayer.VideoPlayer
+import com.metacto.core.utils.PhoneNumberVisualTransformation
+import com.metacto.core.utils.extensions.rememberPhoneNumberUtil
+import com.metacto.core.utils.phoneNumber.IPhoneNumberManager
 import com.sampleApp.app.presentation.home.HomeContract.Event
 import com.sampleApp.app.presentation.home.HomeContract.State
 import com.sampleApp.app.presentation.theme.AppTheme
+import org.koin.compose.koinInject
 
 @Composable
 internal fun HomeContent(
@@ -142,6 +147,45 @@ internal fun HomeContent(
             startIconVector = Icons.Default.Work,
             text = "",
             onClick = {}
+        )
+
+        var phoneNumber by remember { mutableStateOf("") }
+//        val phoneNumberUtil = rememberPhoneNumberUtil()
+//        val phoneNumberVisualTransformation = remember {
+//            PhoneNumberVisualTransformation(phoneNumberUtil)
+//        }
+        val phoneNumberManager = koinInject<IPhoneNumberManager>()
+        PrimaryTextInputField(
+            label = "Phone number",
+//            visualTransformation = phoneNumberVisualTransformation,
+            modifier = Modifier.fillMaxWidth(),
+            text = phoneNumber,
+            onValueChange = {
+                phoneNumber = it
+            }
+        )
+        var countryCode by remember { mutableStateOf("US") }
+        PrimaryTextInputField(
+            label = "Country Code",
+            modifier = Modifier.fillMaxWidth(),
+            text = countryCode,
+            onValueChange = {
+                countryCode = it
+            }
+        )
+        PrimaryFilledButton(
+            modifier = Modifier.fillMaxWidth(),
+            text = "Do Phone number magic!",
+            onClick = {
+                val validPhoneNumber = phoneNumberManager.getValidPhoneNumber(phoneNumber, countryCode)
+                println("PhoneNumberManager -- validPhoneNumber: $validPhoneNumber")
+                val isValid = phoneNumberManager.isValidPhoneNumber(phoneNumber, countryCode)
+                println("PhoneNumberManager --isValid: $isValid")
+                val formattedPhoneNumber = phoneNumberManager.getFormattedPhoneNumber(phoneNumber, countryCode)
+                println("PhoneNumberManager --formattedPhoneNumber: $formattedPhoneNumber")
+                val getE164FormattedPhoneNumber = phoneNumberManager.getE164FormattedPhoneNumber(phoneNumber, countryCode)
+                println("PhoneNumberManager --getE164FormattedPhoneNumber: $getE164FormattedPhoneNumber")
+            }
         )
     }
 }
