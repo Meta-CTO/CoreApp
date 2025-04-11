d9b8451fb8cbf1f92d118f7b16adee299d536f77
Ahmed Salem Elzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-04-11 14:45:40 +0200

    Update Versions.kt

--- buildSrc/src/main/kotlin/Versions.kt
+++ buildSrc/src/main/kotlin/Versions.kt
@@ -10,7 +10,7 @@ object Versions {
     const val GOOGLE_SIGN_IN_POD = "7.0.0"
     const val DATE_TIME = "0.4.0"
     const val COLLECTIONS = "0.3.7"
-    const val STRAPI_KMM = "10.0.143"
+    const val STRAPI_KMM = "10.0.144"
     const val KOIN = "4.0.0"
     const val KOIN_COMPOSE = "1.1.0"
     const val VOYAGER = "1.1.0-beta02"
