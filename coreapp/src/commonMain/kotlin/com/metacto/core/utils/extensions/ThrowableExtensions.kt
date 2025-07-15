c244619014106fd98e0807d992b2ed9d74e91ea0
abdulrahmansaid <abdulrahman.said.bassuony@gmail.com>
2025-07-16 00:09:01 +0300

    Add network error handling and update error messages

--- coreapp/src/commonMain/kotlin/com/metacto/core/utils/extensions/ThrowableExtensions.kt
+++ coreapp/src/commonMain/kotlin/com/metacto/core/utils/extensions/ThrowableExtensions.kt
@@ -4,6 +4,17 @@ import com.metacto.core.presentation.base.getErrorCode
 import com.metacto.strapikmm.errorhandling.AppException
 import com.metacto.strapikmm.errorhandling.NetworkMapperConstants
 
+val INTERNET_ERROR_RANGE = -1000 downTo -1005
+const val NETWORK_CONNECTION_LOST = "network connection was lost"
+
 fun Throwable.isInternetConnectionError(): Boolean {
     return this is AppException && this.getErrorCode() == NetworkMapperConstants.NO_INTERNET_CONNECTION
+}
+
+fun Throwable.isInternetInterruptedError(): Boolean {
+    return this is AppException && this.getErrorCode() in INTERNET_ERROR_RANGE
+}
+
+fun Throwable.isNetworkConnectionLostError(): Boolean {
+    return this is AppException && this.message == NETWORK_CONNECTION_LOST
 }
