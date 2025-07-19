e63140cae119f7fa757441c2a77b7bb3856dcdbe
Ahmed Salem Elzeiny <ahmedsalemelzeiny2013@gmail.com>
2025-07-19 23:39:46 +0300

    Fix core version

--- buildSrc/src/main/kotlin/Configs.kt
+++ buildSrc/src/main/kotlin/Configs.kt
@@ -4,7 +4,7 @@ object Configs {
     const val PUBLISH_REPO_TOKEN = "PUBLISH_REPO_TOKEN"
     const val LOCAL_PROPERTIES = "local.properties"
     const val VERSIONS_PROPERTIES = "versions.properties"
-    const val PUBLISH_VERSION = "PUBLISH_VERSION"
+    const val PUBLISH_VERSION = "PUBLISH_CORE_VERSION"
     const val MAVEN_URL = "https://maven.pkg.github.com/Meta-CTO/coreapp"
     const val PUBLISH_MAVEN_REPO_NAME = "Github"
     const val SH_USERNAME = "developer-swensonhe"
