36336c773ae8829eee3a74a22a7c956ecb9b83ad
Mahmoud Elshamy <shamyyoun@gmail.com>
2025-07-19 21:32:44 +0400

    Add VideoPlayerManager dependency for iOS and bump publish version to 0.1.1-alpha

--- ui/media-players/src/iosMain/kotlin/com/metacto/core/ui/mediaplayers/di/Di.ios.kt
+++ ui/media-players/src/iosMain/kotlin/com/metacto/core/ui/mediaplayers/di/Di.ios.kt
@@ -1,7 +1,12 @@
 package com.metacto.core.ui.mediaplayers.di
 
+import com.metacto.core.ui.mediaplayers.videoPlayer.VideoPlayerManager
 import org.koin.dsl.module
 
 internal actual fun platformModule() = module {
     // iOS specific dependencies can be added here
+
+    single<MutableMap<String, VideoPlayerManager>>(DiQualifiers.videoPlayerManagers) {
+        mutableMapOf()
+    }
 }
